a1a275c386840ff6d32bc21937b629933dba70c0
Wiktor Garbarek <wektorgarbarek@gmail.com>
2017-07-07 00:06:59 +0200

    Added possibility of drawing one frame of game

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -13,14 +13,14 @@ import scala.util.Random
 object InBound{
   def row(number: Int): Int =
     number match {
-      case (-1) => number + 480
-      case 480 => number - 480
+      case (-1) => number + 120
+      case 120 => number - 120
       case n => n
     }
   def column(number: Int): Int =
     number match {
-      case (-1) => number + 640
-      case 640 => number - 640
+      case (-1) => number + 160
+      case 160 => number - 160
       case n => n
     }
 }
@@ -30,7 +30,7 @@ case class Rules(alive_to_alive: List[Int], dead_to_alive: List[Int])
 case class GameTable(table: Array[Array[Int]], rules: Rules){
   def update : GameTable = {
     var copied : Array[Array[Int]] = table;
-    for (i <- 0.to(479); j <- 0.to(639)){
+    for (i <- 0.to(119); j <- 0.to(159)){
       var neighbours: Int = List(
           table(InBound.row(i-1))(InBound.column(j-1)),
           table(InBound.row(i-1))(j),
@@ -55,7 +55,7 @@ case class GameTable(table: Array[Array[Int]], rules: Rules){
 }
 
 object RandomValue{
-  def get = {
+  def get: Int = {
     val generator: List[Int] = List(0,0,0,0,0,0,0,1,1,1)
     Random.shuffle(generator).head
   }
@@ -64,6 +64,24 @@ object RandomValue{
 object TableGenerator{
   def generate: GameTable =
     new GameTable(
-      1.to(480).toArray.map { (_ : Int) => 1.to(640).toArray.map { (_ : Int) => RandomValue.get} },
+      1.to(120).toArray.map { (_ : Int) => 1.to(160).toArray.map { (_ : Int) => RandomValue.get} },
       new Rules(List(1,2), List(3,4)) )
+}
+object Show{
+  def draw_cell(x: Int): Image =
+    x match {
+      case 1 => square(3) fillColor Color.blue
+      case 0 => square(3) fillColor Color.white
+      }
+  def draw_row(array: Array[Int]): Image =
+    array match {
+      case Array() => Image.empty
+      case _ => draw_cell(array.head) beside draw_row(array.tail)
+
+    }
+  def draw(table: Array[Array[Int]]): Image =
+    table match {
+      case Array() => Image.empty
+      case _ => draw_row(table.head) below draw(table.tail)
+    }
 }
